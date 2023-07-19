703f1a7a494a035b027c71167410d18f95be319d
Shyam Sunder Prajapat <shyamsunderprajapat.nita@gmail.com>
2023-07-20 00:11:01 +0530

    Added delete button for deleting the blog.

--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -2,17 +2,18 @@ const BlogList = (props) => {
     const blogs = props.blogs;
     const title = props.title;
     // const BlogList = ({blogs, title}) => {   //This is also the way to provide props by destructuring it in the argument itself.
-    return ( 
+    return (
         <div className="blog-list">
-        <h2>{title}</h2>
+            <h2>{title}</h2>
             {blogs.map((blog) => (
                 <div className="blog-preview" key={blog.id}>
                     <h2>{blog.title}</h2>
                     <p>Written by : {blog.author}</p>
+                    <button onClick={() => props.handleDelete(blog.id)}>Delete Blog</button>
                 </div>
             ))}
         </div>
-     );
+    );
 }
- 
+
 export default BlogList;
--- src/components/Home.js
+++ src/components/Home.js
@@ -10,11 +10,16 @@ const Home = () => {
         {title: '3rd Blog', author: 'Person 3', body: 'fjnjr arroa...', id: 3}
     ]);
 
+    const handleDelete = (id) => {
+        let newBlogs = blogs.filter(blogie => blogie.id !== id)
+        setBlogs(newBlogs);
+    }
+
     return (
         <div className="home">
-            <BlogList blogs={blogs} title="All Blogs!!" />
+            <BlogList blogs={blogs} title="All Blogs!!" handleDelete={handleDelete} />
             {/* The method below filter's only the required blog example for searching a blog */}
-            {/* <BlogList blogs={blogs.filter((blog) => blog.author === 'Person 2')} title="Person 2's Blogs!!" />  */}
+            {/* <BlogList blogs={blogs.filter((blog) => blog.author === 'Person 2')} title="Person 2's Blogs!!" handleDelete={handleDelete} />  */}
         </div>
     );
 }
