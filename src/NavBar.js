bfc22a568152e7931c1d5a8102b7d67441144933
Shyam Sunder Prajapat <shyamsunderprajapat.nita@gmail.com>
2023-07-19 18:42:35 +0530

    Initial commit for starting the repo.

---
+++ src/NavBar.js
@@ -0,0 +1,13 @@
+const NavBar = () => {
+    return ( 
+        <nav className="navbar">
+            <h1>Welcome Bloggers to Open Blogs</h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/create">Fresh Blog</a>
+            </div>
+        </nav>
+     );
+}
+ 
+export default NavBar;
