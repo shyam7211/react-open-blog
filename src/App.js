30bbaec1465e8d84243e04fcb385805af9c5effe
Shyam Sunder Prajapat <shyamsunderprajapat.nita@gmail.com>
2023-07-21 22:25:16 +0530

    Created Create.js component and added routes.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
-import NavBar from './components/NavBar';
+import Create from './components/Create';
 import Home from './components/Home';
+import NavBar from './components/NavBar';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <NavBar /> 
-      <div className="content">
-        <Home />
+    <Router>
+      <div className="App">
+        <NavBar />
+        <div className="content">
+          <Routes>
+            <Route exact path='/' element={<Home />} />
+            <Route exact path='/create' element={<Create />} />
+          </Routes>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
---
+++ src/components/Create.js
@@ -0,0 +1,9 @@
+const Create = () => {
+    return ( 
+        <div className="create">
+            Create the blog.
+        </div>
+     );
+}
+ 
+export default Create;
