ae0d8caad82df496438c25e782d047c39ebd8163
Shyam Sunder Prajapat <shyamsunderprajapat.nita@gmail.com>
2023-07-22 13:44:55 +0530

    Added active class to navbar and Link to NavLink.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,13 @@
+import { Link, NavLink } from "react-router-dom";
+
+
 const NavBar = () => {
     return ( 
         <nav className="navbar">
             <h1>Welcome Bloggers to Open Blogs</h1>
             <div className="links">
-                <a href="/">Home</a>
-                <a href="/create">Fresh Blog</a>
+                <NavLink to="/" activeClassName="active" >Home</NavLink>
+                <NavLink to="/create" activeClassName="active" >Fresh Blog</NavLink>
             </div>
         </nav>
      );
